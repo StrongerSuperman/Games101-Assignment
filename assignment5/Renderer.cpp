8ec8cf18265b1de73ca7513d7f0608782bb7979c
chenxinyu <515785125@qq.com>
2021-11-15 21:59:27 +0800

    commit assignment5

--- assignment5/Renderer.cpp
+++ assignment5/Renderer.cpp
@@ -222,15 +222,31 @@ void Renderer::Render(const Scene& scene)
     {
         for (int i = 0; i < scene.width; ++i)
         {
-            // generate primary ray direction
-            float x;
-            float y;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
-            // x (horizontal) variable with the *imageAspectRatio*            
+            // x (horizontal) variable with the *imageAspectRatio*     
+
+            // generate primary ray direction
+            // Screen space to NDC space
+            float nx = (i + 0.5f) * 2 / scene.width - 1.0f;
+            float ny = (j + 0.5f) * 2 / scene.height - 1.0f;
+
+            // NDC space to view space
+
+            // Project matrix
+            /*
+            *   [ n/r ,0   ,0       ,0      ]
+            *   [ 0   ,n/t ,0       ,0      ]
+            *   [ 0   ,0   ,n+f/n-f ,2nf/f-n]
+            *   [ 0   ,0   ,1       ,0      ]
+            */
+
+            float x = nx * scale * imageAspectRatio;
+            float y = -ny * scale;
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
--- assignment5/Triangle.hpp
+++ assignment5/Triangle.hpp
@@ -11,6 +11,25 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+
+    /* 
+     * Solve equation : o + dt = x0 * (1 - s - t) + x1 * s + x2 * t
+     * [Moller-Trumbore]
+     */ 
+    auto e1 = v1 - v0, e2 = v2 - v0, s = orig - v0;
+    auto s1 = crossProduct(dir, e2), s2 = crossProduct(s, e1);
+    float s1_dot_e1 = dotProduct(s1, e1);
+    float t = dotProduct(s2, e2) / s1_dot_e1;
+    float b1 = dotProduct(s1, s) / s1_dot_e1;
+    float b2 = dotProduct(s2, dir) / s1_dot_e1;
+    if (t > 0.0 && b1 > 0.0 && b2 > 0.0 && (1 - b1 - b2) > 0.0)
+    {
+        tnear = t;
+        u = b1;
+        v = b2;
+
+        return true;
+    }
     return false;
 }
 
